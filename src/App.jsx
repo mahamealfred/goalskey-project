90a16c1ea055f8323ade3f1933b775fe866a8dad
mahamealfred <mahamealfred@gmail.com>
2024-01-11 19:05:27 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import Footer from "./components/Footer/Footer";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Blogs from "./pages/Blogs";
+import NoPage from "./pages/NoPage";
+import PlacesRoute from "./pages/PlacesRoute";
+import About from "./pages/About";
+import BlogsDetails from "./pages/BlogsDetails";
+function App() {
+  const [orderPopup, setOrderPopup] = React.useState(false);
+
+  const handleOrderPopup = () => {
+    setOrderPopup(!orderPopup);
+  };
+
+  React.useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
+
+  return (
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="blogs" element={<Blogs />} />
+          <Route path="blogs/:id" element={<BlogsDetails />} />
+          <Route path="best-places" element={<PlacesRoute />} />
+          <Route path="about" element={<About />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </>
+  )
+}
+
+export default App
